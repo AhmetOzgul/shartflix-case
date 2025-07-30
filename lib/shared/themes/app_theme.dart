01b6d91be30ea4f281c3432a25cae817969222a1
Ahmet Özgül <21060682@stu.omu.edu.tr>
2025-07-30 19:11:41 +0300

    feat: implement localization support and themes

---
+++ lib/shared/themes/app_theme.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFFE50914);
+  static const Color secondaryColor = Colors.orange;
+  static const Color backgroundColor = Colors.black;
+  static const Color surfaceColor = Color(0xFF212120);
+  static const Color textColor = Colors.white;
+  static const Color textError = Colors.red;
+  static const String fontFamily = 'EuclidCircularA';
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      fontFamily: fontFamily,
+      primaryColor: primaryColor,
+      scaffoldBackgroundColor: backgroundColor,
+      colorScheme: const ColorScheme.light(
+        primary: primaryColor,
+        secondary: secondaryColor,
+        background: backgroundColor,
+        surface: surfaceColor,
+        onPrimary: textColor,
+        onSecondary: textColor,
+        onBackground: textColor,
+        onSurface: textColor,
+      ),
+      textTheme: TextTheme(
+        displayMedium: TextStyle(
+          color: textColor,
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+        ),
+        titleLarge: TextStyle(
+          color: textColor,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+        titleMedium: TextStyle(
+          color: textColor,
+          fontSize: 15,
+          fontWeight: FontWeight.w600,
+        ),
+        titleSmall: TextStyle(
+          color: textColor,
+          fontSize: 13,
+          fontWeight: FontWeight.w600,
+        ),
+        bodyLarge: TextStyle(
+          color: textColor,
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+        ),
+        bodyMedium: TextStyle(
+          color: textColor,
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+        ),
+        bodySmall: TextStyle(
+          color: textColor,
+          fontSize: 13,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      fontFamily: fontFamily,
+      primaryColor: primaryColor,
+      scaffoldBackgroundColor: backgroundColor,
+      colorScheme: const ColorScheme.dark(
+        primary: primaryColor,
+        secondary: secondaryColor,
+        background: backgroundColor,
+        surface: surfaceColor,
+        onPrimary: textColor,
+        onSecondary: textColor,
+        onBackground: textColor,
+        onSurface: textColor,
+      ),
+      textTheme: TextTheme(
+        displayMedium: TextStyle(
+          color: textColor,
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+        ),
+        titleLarge: TextStyle(
+          color: textColor,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+        titleMedium: TextStyle(
+          color: textColor,
+          fontSize: 15,
+          fontWeight: FontWeight.w600,
+        ),
+        titleSmall: TextStyle(
+          color: textColor,
+          fontSize: 13,
+          fontWeight: FontWeight.w600,
+        ),
+        bodyLarge: TextStyle(
+          color: textColor,
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+        ),
+        bodyMedium: TextStyle(
+          color: textColor,
+          fontSize: 15,
+          fontWeight: FontWeight.w500,
+        ),
+        bodySmall: TextStyle(
+          color: textColor,
+          fontSize: 13,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+}
